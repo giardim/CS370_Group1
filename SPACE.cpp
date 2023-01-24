f73c36d996385328838597bde9c20969ed925767
Michael Giardina <mgiardina02@gmail.com>
2023-01-24 11:05:02 -0500

    Added way to save file as json

--- SPACE.cpp
+++ SPACE.cpp
@@ -1,14 +1,31 @@
 #include <iostream>
 #include <cstdlib>
+#include <fstream>
 #include "SPACE.h"
 
     SPACE::SPACE(int* array, int size)
-    {
+    {   
+        std::ofstream filej;
+
+        filej.open("SPACE.json");
+
+        filej << "{\n";
+
         a = new int[size];
         for (int i = 0; i < size; i++){
             this->a[i] = array[i];
         }
         for (int i = 0; i < size; ++i){
-            std::cout << a[i];
+            filej << "\"Array" << i << "\": \"";
+            for (int j = 0; j < size; ++j){
+                filej << a[j];
+            }
+            if (i == size - 1){
+                filej << "\"\n";
+            }else{
+            filej << "\",\n";
+            }
         }
+        
+        filej << "}";
     };
