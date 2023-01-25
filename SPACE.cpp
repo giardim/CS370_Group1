ea9d494803418275707da0f7030aed22cb164711
Michael Giardina <mgiardina02@gmail.com>
2023-01-25 15:38:23 -0500

    Fixed CSV format

--- SPACE.cpp
+++ SPACE.cpp
@@ -18,6 +18,9 @@
 
         for (int i = 0; i < size; ++i){
             file << a[i];
+            if (i != size - 1){
+                file << ",";
+            }
         }
         
         file << "\n";
